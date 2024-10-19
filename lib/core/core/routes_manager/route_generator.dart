9a215d9c50740a68ff1af1eb9374e8feb1c37a70
mayaryasser2005 <mayaryasser300@gmail.com>
2024-10-20 01:48:09 +0300

    Initial commit.

---
+++ lib/core/core/routes_manager/route_generator.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce_app/core/core/routes_manager/routes.dart';
+import 'package:flutter/material.dart';
+
+import '../../../features/features/auth/presentation/screens/sign_in_screen.dart';
+import '../../../features/features/auth/presentation/screens/sign_up_screen.dart';
+import '../../../features/features/cart/screens/cart_screen.dart';
+import '../../../features/features/main_layout/main_layout.dart';
+import '../../../features/features/product_details/presentation/screen/product_details.dart';
+import '../../../features/features/products_screen/presentation/screens/products_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.cartRoute:
+        return MaterialPageRoute(builder: (_) => const CartScreen());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainLayout());
+
+      case Routes.productsScreenRoute:
+        return MaterialPageRoute(builder: (_) => const ProductsScreen());
+
+      case Routes.productDetails:
+        return MaterialPageRoute(builder: (_) => const ProductDetails());
+
+      case Routes.signInRoute:
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
+
+      case Routes.signUpRoute:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No Route Found'),
+        ),
+        body: const Center(child: Text('No Route Found')),
+      ),
+    );
+  }
+}
